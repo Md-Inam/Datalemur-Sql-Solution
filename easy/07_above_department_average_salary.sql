30c46acad3b7dbe37258ec9f6c67282ff3f7ff2d
Übermensch <149220325+Md-Inam@users.noreply.github.com>
2026-08-14 18:11:26 +0530

    Delete easy/07_above_department_average_salary.sql

--- easy/07_above_department_average_salary.sql
+++
@@ -1,9 +0,0 @@
-﻿-- Question: Employees Earning Above Their Own Department Average
--- Difficulty: Easy-Medium
-SELECT emp_id, salary, department
-FROM employees e1
-WHERE salary > (
-    SELECT AVG(salary)
-    FROM employees e2
-    WHERE e2.department = e1.department
-);
