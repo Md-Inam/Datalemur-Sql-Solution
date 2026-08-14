a1fdfd0c2aa96cb0735adda4a8b96e5e561557f7
Übermensch <149220325+Md-Inam@users.noreply.github.com>
2026-08-14 18:11:17 +0530

    Delete easy/01_employee_manager_selfjoin.sql

--- easy/01_employee_manager_selfjoin.sql
+++
@@ -1,8 +0,0 @@
-﻿-- Question: Employee & Manager (Self-Join)
--- Difficulty: Easy
-SELECT
-    e1.name AS employee_name,
-    e2.name AS manager_name
-FROM employees e1
-JOIN employees e2
-    ON e1.manager_id = e2.emp_id;
